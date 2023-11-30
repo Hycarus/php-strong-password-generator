cf7b9d1f393efceda1d817b8ff82cf3766002e05
Hycarus <mirkomarasco08@gmail.com>
2023-11-30 15:26:17 +0100

    refactoring header.php

--- partials/header.php
+++ partials/header.php
@@ -37,25 +37,6 @@ include __DIR__ . '/../functions/functions.php';
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.php">About</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
                     </ul>
                     <?php if (!empty($_SESSION['auth_token'])) { ?>
                         <a href="logout.php" class="btn btn-primary ">Logout</a>
